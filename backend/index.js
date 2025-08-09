79613e38b7271de952b96e3c07510155261a3190
AshitoshAmbilwade <ashitoshambilwade1289@gmail.com>
2025-08-10 00:32:01 +0530

    DONE

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`)});
+
 // Connect DB
 connectDB();
 
